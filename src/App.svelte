8c4d073939c1478a29954b7f088176f3c2852356
SvenjaJeschke <svenja.jeschke12@gmail.com>
2020-05-05 18:03:19 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import ToolsList from './components/ToolsList.svelte';
+	export let tools;
+</script>
+
+<main>
+	<h1>Herzlich Willkommen!</h1>
+	<p>
+		Auf dieser Webseite werden Ihnen mehrere Programme und Tools zum Thema Online-Konferenzen vorgestellt.<br>
+		Hier finden Sie detaillierte Beschreibungen, Vergleiche und Anleitungen zu den verschiedenen Tools.
+	</p>
+	<ToolsList tools={tools} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #80f9ff;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	p {
+		margin-bottom: 75px;
+	}
+
+	:global(body) {
+		background-color: #232323;
+		color: #c8c8c8;
+	}
+</style>
---
+++ src/components/ToolsList.svelte
@@ -0,0 +1,96 @@
+<script>
+    export let tools;
+
+    function displayPrice(price) {
+        price = parseFloat(price);
+        if (price > 0) {
+            return `${price} €`;
+        }
+        return 'kostenlos';
+    }
+</script>
+
+<div class="flex-wrapper">
+    {#each tools as tool}
+        <div class="card" style="background-color: {tool.color}">
+            <div class="card-title">
+                <div class="heading">
+                    {tool.displayName}
+                </div>
+                <button class="icon-button">
+                    <i class="fas fa-info"></i>
+                </button>
+            </div>
+            <div class="card-content">
+                <ul>
+                    <li>
+                        <b>Mindestpreis:</b> {displayPrice(tool.minPricePerMonth)}
+                    </li>
+                    <li>
+                        <b>Maximale Teilnehmer:</b> {tool.maxParticipants}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .card {
+        flex-basis: 30%;
+        border-radius: 5px;
+        margin-bottom: 10px;
+        box-shadow: 4px 4px 10px 0px rgba(0,0,0,0.5);
+        color: rgba(255, 255, 255, 0.8);
+    }
+    .heading {
+        font-size: x-large;
+        padding-left: 20px;
+    }
+    .card-title {
+        text-align: start;
+        padding: 10px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .card-content {
+        text-align: start;
+        padding: 5px;
+    }
+    hr {
+        background-color: #8c8c8c;
+        border: 0 none;
+        color: #8c8c8c;
+        height: 1px;
+    }
+    .icon-button {
+        background-color: rgba(255, 255, 255, 0);
+        border: none;
+        color: white;
+        flex-basis: 30px;
+        border-radius: 7px;
+        background-position: center;
+        transition: background 0.5s;
+    }
+    .icon-button:hover {
+        background-color: rgba(255, 255, 255, 0.3);
+        cursor: pointer;
+        background: rgba(255, 255, 255, 0.4) radial-gradient(circle, transparent 1%, rgba(255, 255, 255, 0.1) 1%) center/15000%;
+    }
+    .icon-button:active {
+        cursor: pointer;
+        background-size: 100%;
+        transition: background 0s;
+    }
+    .icon-button:focus {
+        background-color: rgba(255, 255, 255, 0.3);
+        outline: 0;
+    }
+    @media(min-width: 900px) {
+        .flex-wrapper {
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+        }
+    }
+</style>
